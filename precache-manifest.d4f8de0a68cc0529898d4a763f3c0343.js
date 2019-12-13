4d530db08b64ffebee98a017d60fdda60c1e5cac
dandagrizzly <stavenessd2012@my.uwstout.edu>
2019-12-13 16:47:32 -0600

    Updates

---
+++ precache-manifest.d4f8de0a68cc0529898d4a763f3c0343.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ae4bee8d3975e79a940cc6df560802f",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "9ea9d4fc36fa3ddd29d4",
+    "url": "/myportfolio/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "61216d610e2eb012bd55",
+    "url": "/myportfolio/static/css/main.66a4b34d.chunk.css"
+  },
+  {
+    "revision": "9ea9d4fc36fa3ddd29d4",
+    "url": "/myportfolio/static/js/2.0318cd52.chunk.js"
+  },
+  {
+    "revision": "61216d610e2eb012bd55",
+    "url": "/myportfolio/static/js/main.6357c008.chunk.js"
+  },
+  {
+    "revision": "947b741494b1ab06f85b",
+    "url": "/myportfolio/static/js/runtime-main.c840c0d7.js"
+  },
+  {
+    "revision": "44be1ccd70f33558088c75756c331dd3",
+    "url": "/myportfolio/static/media/facebook.44be1ccd.png"
+  },
+  {
+    "revision": "338901b594ed6c32c569e506cfa07c21",
+    "url": "/myportfolio/static/media/me.338901b5.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pyportfolio/precache-manifest.eb5e1a7829b6b5522c263817cb1f18ee.js"
+  "/myportfolio/precache-manifest.d4f8de0a68cc0529898d4a763f3c0343.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pyportfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myportfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

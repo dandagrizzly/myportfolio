8fed5f28547e8fb902aad3431fdc6ed16f6334bf
dandagrizzly <stavenessd2012@my.uwstout.edu>
2019-12-13 16:45:37 -0600

    Updates

---
+++ precache-manifest.9950f65bef5447905807bec4321175a2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3c7dbf1e8c605a86679a5c32f03e7db",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "9ea9d4fc36fa3ddd29d4",
+    "url": "/react-gh-pages/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "feb3618f695275a0cb26",
+    "url": "/react-gh-pages/static/css/main.66a4b34d.chunk.css"
+  },
+  {
+    "revision": "9ea9d4fc36fa3ddd29d4",
+    "url": "/react-gh-pages/static/js/2.0318cd52.chunk.js"
+  },
+  {
+    "revision": "feb3618f695275a0cb26",
+    "url": "/react-gh-pages/static/js/main.597ede16.chunk.js"
+  },
+  {
+    "revision": "2392956db7cde22041e6",
+    "url": "/react-gh-pages/static/js/runtime-main.ea11254a.js"
+  },
+  {
+    "revision": "44be1ccd70f33558088c75756c331dd3",
+    "url": "/react-gh-pages/static/media/facebook.44be1ccd.png"
+  },
+  {
+    "revision": "338901b594ed6c32c569e506cfa07c21",
+    "url": "/react-gh-pages/static/media/me.338901b5.png"
+  }
+]);
